333ca6fa8dc575b22128dec2cf073c6e2808a11d
Elizabeth <elzbthyorke@gmail.com>
2022-02-14 13:50:06 -0600

    February 14, 2022, 1:50 pm

--- script.js
+++ script.js
@@ -1,35 +1,100 @@
 // Write your JavaScript code here!
 
-// const { formSubmission } = require("./scriptHelper");
+const { myFetch } = require("./scriptHelper");
 
 
 window.addEventListener("load", function() {
 
-    // console.log("Hello harry");
-    // console.log( { formSubmission } );
-
     let form = document.querySelector("form");
 
 
+    //when working with Angela we put ".value", after all of pilot thru cargoLevel, to get the user input, but now going thru and console logging them, they're blank becuase on the window load they are not inputed by the user yet, but because they are const, i can't reassign them as new variables?, I just don't know hwo I know that theses are working
+    const list = document.getElementById("faultyItems");
+    const pilot = document.querySelector("input[name=pilotName]");
+    const copilot = document.querySelector("input[name=copilotName]");
+    const fuelLevel = document.querySelector("input[name=fuelLevel]");
+    const cargoLevel = document.querySelector("input[name=cargoMass]");
 
-    let pilot = document.querySelector("input[name=pilotName]");
-    let copilot = document.querySelector("input[name=copilotName]");
-    let fuelLevel = document.querySelector("input[name=fuelLevel]");
-    let cargoMass = document.querySelector("input[name=cargoMass]");
+    const launchStatus = document.getElementById("launchStatus")
+
 
     form.addEventListener("submit", function (event){
+        
+        // working with formSubmission function here we use the event prevent default
+        event.preventDefault();
 
-        console.log(pilot.value);
-        console.log(copilot.value);
-        console.log(fuelLevel.value);
-        console.log(cargoMass.value);
 
-        alert("Are you ready to submit?");
+        formSubmission("document", list, pilot.value, copilot.value, fuelLevel.value, cargoLevel.value);
+        
+
+        if (Number(fuelLevel.value) < 10000 ){
+            console.log("Fuel Level low");
+            list.style.visibility = "visible";
+            list.innerHTML = `
+                <ol>
+                    <li id="pilotStatus" data-testid="pilotStatus">${pilot.value}</li>
+                    <li id="copilotStatus" data-testid="copilotStatus">${copilot.value}</li>
+                    <li id="fuelStatus" data-testid="fuelStatus">There is not enough fuel for the journey.</li>
+                    <li id="cargoStatus" data-testid="cargoStatus">${cargoLevel.value} kg</li>
+                </ol>
+            `;
+            launchStatus.innerHTML = `Shuttle not ready for launch`;
+            launchStatus.style.color = "red";
+        }
+        if (Number(cargoLevel.value) > 10000){
+            console.log("Cargo Mass too high");
+            list.style.visibility = "visible";
+            list.innerHTML = `
+                <ol>
+                    <li id="pilotStatus" data-testid="pilotStatus">${pilot.value}</li>
+                    <li id="copilotStatus" data-testid="copilotStatus">${copilot.value}</li>
+                    <li id="fuelStatus" data-testid="fuelStatus">${fuelLevel.value} liters</li>
+                    <li id="cargoStatus" data-testid="cargoStatus">There is too much mass for the shuttle to take off.</li>
+                </ol>
+            `;
+            launchStatus.innerHTML = `Shuttle not ready for launch`;
+            launchStatus.style.color = "red";
+        }
+
+        if (Number(cargoLevel.value) > 10000 && Number(cargoLevel.value) > 10000){
+            console.log("Cargo Mass too high");
+            list.style.visibility = "visible";
+            list.innerHTML = `
+                <ol>
+                    <li id="pilotStatus" data-testid="pilotStatus">${pilot.value}</li>
+                    <li id="copilotStatus" data-testid="copilotStatus">${copilot.value}</li>
+                    <li id="fuelStatus" data-testid="fuelStatus">There is not enough fuel for the journey.</li>
+                    <li id="cargoStatus" data-testid="cargoStatus">There is too much mass for the shuttle to take off.</li>
+                </ol>
+            `;
+            launchStatus.innerHTML = `Shuttle not ready for launch`;
+            launchStatus.style.color = "red";
+        }
+        
+        if (Number(cargoLevel.value) < 10000 && Number(cargoLevel.value) < 10000 && formSubmission("document", list, pilot.value, copilot.value, fuelLevel.value, cargoLevel.value) === "Validated"){
+            console.log("update Html elements here for ordered list.");
+            console.log(list);
+            list.style.visibility = "visible";
+            list.innerHTML = `
+                <ol>
+                    <li id="pilotStatus" data-testid="pilotStatus">${pilot.value}</li>
+                    <li id="copilotStatus" data-testid="copilotStatus">${copilot.value}</li>
+                    <li id="fuelStatus" data-testid="fuelStatus">${fuelLevel.value} liters</li>
+                    <li id="cargoStatus" data-testid="cargoStatus">${cargoLevel.value} kg</li>
+                </ol>
+            `;
+            launchStatus.innerHTML = `Shuttle is ready for launch`;    
+            launchStatus.style.color = "green";
+        }
+
+
+
+
     })
 
    let listedPlanets;
    // Set listedPlanetsResponse equal to the value returned by calling myFetch()
-   let listedPlanetsResponse;
+   let listedPlanetsResponse = myFetch();
    listedPlanetsResponse.then(function (result) {
        listedPlanets = result;
        console.log(listedPlanets);
--- scriptHelper.js
+++ scriptHelper.js
@@ -14,27 +14,60 @@ function addDestinationInfo(document, name, diameter, star, distance, moons, ima
                 </ol>
                 <img src="">
    */
+    let document = document.getElementById("missionTarget");
+    document.innerHtml = `
+        <h2>Mission Destination</h2>
+        <ol>
+            <li>Name: ${name}</li>
+            <li>Diameter: ${diameter}</li>
+            <li>Star: ${star}</li>
+            <li>Distance from Earth: ${distance}</li>
+            <li>Number of Moons: ${moons}</li>
+        </ol>
+        <img src="${imageUrl}">
+    `
 }
 
 function validateInput(testInput) {
-   
+    if (isNaN(testInput)){
+        return "Not a number"; 
+    } else if (testInput === ""){
+        return "Empty";
+    } else if (!isNaN(testInput)){
+        return 'Is a Number'
+    };
 }
-
+// document stands for the for and list for the "faulty items" (the items aren't passing the Shuttle Requirements)
 function formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel) {
-    return "complete";
+
+    if (validateInput(pilot) == "Empty" || validateInput(copilot) == "Empty" || validateInput(fuelLevel) == "Empty" || validateInput(cargoLevel) == 
+    "Empty"){
+        alert("All Fields are Required.");
+    }else if (validateInput(fuelLevel) == "Not a number" || validateInput(cargoLevel) == "Not a number"){
+        alert ("Please enter a Number for Fuel Level and Cargo Mass.");
+    }
+
+    else{
+        return "Validated";
+    }
    
 }
 
 async function myFetch() {
     let planetsReturned;
 
-    planetsReturned = await fetch().then( function(response) {
-        });
+    planetsReturned = await fetch("https://handlers.education.launchcode.org/static/planets.json").then( function(response) {
+        response.json().then(function(json)){
+            // call addDestinationInfo function or call pickPlanet Function?
+        }        
+});
 
     return planetsReturned;
 }
 
 function pickPlanet(planets) {
+    let index = Math.round(((planets.length-1)/10) * Math.round(Math.random()*10))
+    return planets[index];
 }
 
 module.exports.addDestinationInfo = addDestinationInfo;
